201c67eeff22c1fce1d5f5d16568d06c1cc561e4
mieum12 <qpdlqltb1215@gmail.com>
2024-02-18 22:16:09 +0900

    fix: 불필요한 코드 제거

--- pages/api/new-post.js
+++ pages/api/new-post.js
@@ -8,7 +8,6 @@ export default async function handler(req, res) {
     // 1. 요청을 받아들이고 데이터베이스에 저장
     // req 객체에는 헤더와 요청 본문이 들어감
     const data = req.body
-    const { title, image, summary, description } = data
     const url = 'mongodb+srv://qpdlqltb1215:ADriB68N9I2u2KaY@cluster0.trp51w4.mongodb.net/mydatabase?retryWrites=true&w=majority'
     const client = await MongoClient.connect(url)
     const db = client.db()
