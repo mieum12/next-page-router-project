8107a60f60c6108fd80d56de3b5afb9b0c480aa7
mieum12 <qpdlqltb1215@gmail.com>
2024-02-18 22:30:20 +0900

    feat: API route로 HTTP 요청 전송하는 코드 구현

--- pages/new-post/index.js
+++ pages/new-post/index.js
@@ -1,8 +1,20 @@
 import NewPostForm from "@/components/posts/NewPostForm";
+import {useRouter} from "next/router";
 
-export default function NewPost() {
-  function addPostHandler (enteredPostData) {
+export default function NewPostPage() {
+  const router = useRouter()
+  async function addPostHandler (enteredPostData) {
     console.log(enteredPostData)
+    const response = await fetch('/api/new-post', {
+      method: 'POST',
+      body: JSON.stringify(enteredPostData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const data = await response.json()
+    console.log(data)
+    await router.push('/')
   }
   return (
     <NewPostForm onAddPost={addPostHandler}/>
